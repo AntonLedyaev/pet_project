cc13406d66b723ff57f12314500b3e6b4c648b10
9787 <9787@mail.ru>
2023-10-06 22:45:44 +0300

    test commit

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,11 +2,11 @@ import {classNames} from "shared/lib/classNames/classNames";
 
 import styles from './ThemeSwitcher.module.scss';
 import React from "react";
-import {Theme, useTheme} from "app/providers/ThemeProvider";
+import { Theme, useTheme } from "app/providers/ThemeProvider";
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
-import {Button} from "shared/Button";
-import {ThemeButton} from "shared/Button/ui/Button";
+import { Button } from "shared/Button";
+import { ThemeButton } from "shared/Button/ui/Button";
 
 
 interface ThemeSwitcherProps {
